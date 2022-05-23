5b351ac6f6616fa2a4a5dfd62eb02cd16c8aab6e
Randall Bruder <randybruder@gmail.com>
2022-05-23 22:17:42 +0000

    comment spacing fix

--- dev/site.js
+++ dev/site.js
@@ -56,5 +56,5 @@ jQuery(document).ready(function($) {
 function getRandomInt(min, max) {
 	min = Math.ceil(min);
 	max = Math.floor(max);
-	return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+	return Math.floor(Math.random() * (max - min + 1)) + min; // The maximum is inclusive and the minimum is inclusive 
 }
